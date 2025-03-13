287085fc370afb99c04d71225ef23b2669b7e114
Sewon Kim <sewonkim1008@gmail.com>
2025-03-13 15:40:29 +0900

    server

--- class_practice/tomcat/my-taro-card/src/main/java/org/example/mytarocard/controller/Controller.java
+++ class_practice/tomcat/my-taro-card/src/main/java/org/example/mytarocard/controller/Controller.java
@@ -1,5 +1,7 @@
 package org.example.mytarocard.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,13 +10,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public abstract class Controller extends HttpServlet {
+    // logger를 구현할 필요는 없음
 
-    protected void view(HttpServletRequest req,
-                        HttpServletResponse resp,String name) throws
-            ServletException, IOException {
-        String prefix="/WEB-INF/views/";
-        String suffix="jsp";
+    protected final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    protected final ObjectMapper objectMapper = new ObjectMapper(); // 직렬화
+    // HttpServlet -> 서버 -> logger
 
-        req.getRequestDispatcher("%s/%s.%s").formatted(prefix,name,suffix)).forward(req,resp);
+    protected void view(HttpServletRequest req, HttpServletResponse resp, String name) throws ServletException, IOException {
+        String prefix = "/WEB-INF/views";
+        String suffix = "jsp";
+        // 접속하려는 주소는 유지한채로 특정한 경로로 이동
+        req.getRequestDispatcher("%s/%s.%s".formatted(prefix, name, suffix)).forward(req, resp);
     }
 }
---
+++ class_practice/tomcat/my-taro-card/src/main/java/org/example/mytarocard/controller/TaroFormController.java
@@ -0,0 +1,49 @@
+package org.example.mytarocard.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.mytarocard.model.dto.LLMServiceParam;
+import org.example.mytarocard.model.dto.LLMServiceResponse;
+import org.example.mytarocard.service.LLMService;
+import org.example.mytarocard.service.LLMServiceImpl;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet("/")
+public class TaroFormController extends Controller {
+    private LLMService llmService;
+
+    @Override
+    public void init() throws ServletException {
+        log("TaroFormController Init");
+        llmService = LLMServiceImpl.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log("TaroFormController doGet");
+        req.setAttribute("pageTitle", "타로 좋아! 정말 좋아!");
+        req.setAttribute("ogTitle", "어렸을 때부터 타로를 좋아한 사람이 있었다?");
+        req.setAttribute("ogDescription", "타로타로 타로 밀크티!!!");
+        req.setAttribute("ogImageUrl", "%s/assets/%s".formatted(req.getContextPath(), "taro.jpeg"));
+        req.setAttribute("ogPageUrl", req.getContextPath());
+        view(req, resp, "form");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log("TaroFormController doPost");
+        LLMServiceParam param = new LLMServiceParam();
+        LLMServiceResponse response = llmService.callModel(param);
+        log(response.data());
+
+        // 겹칠 확률이 극도로 낮은 임의의 문자열 값
+        String uuid = UUID.randomUUID().toString();
+        // 새로운 호출로 처리하겠네...
+
+        resp.sendRedirect(req.getContextPath() + "/result/%s".formatted(uuid));
+    }
+}
