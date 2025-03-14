92012504baff04e52fdb5e48d83ab653184fe4d4
Sewon Kim <sewonkim1008@gmail.com>
2025-03-14 11:29:08 +0900

    LLM 연결

--- class_practice/tomcat/my-taro-card/src/main/java/org/example/mytarocard/controller/TaroFormController.java
+++ class_practice/tomcat/my-taro-card/src/main/java/org/example/mytarocard/controller/TaroFormController.java
@@ -9,6 +9,8 @@ import org.example.mytarocard.model.dto.LLMServiceParam;
 import org.example.mytarocard.model.dto.LLMServiceResponse;
 import org.example.mytarocard.service.LLMService;
 import org.example.mytarocard.service.LLMServiceImpl;
+import org.example.mytarocard.service.SupabaseService;
+import org.example.mytarocard.service.SupabaseServiceImpl;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -16,11 +18,13 @@ import java.util.UUID;
 @WebServlet("/")
 public class TaroFormController extends Controller {
     private LLMService llmService;
+    private SupabaseService supabaseService;
 
     @Override
     public void init() throws ServletException {
         log("TaroFormController Init");
         llmService = LLMServiceImpl.getInstance();
+        supabaseService = SupabaseServiceImpl.getInstance();
     }
 
     @Override
@@ -29,7 +33,7 @@ public class TaroFormController extends Controller {
         req.setAttribute("pageTitle", "타로 좋아! 정말 좋아!");
         req.setAttribute("ogTitle", "어렸을 때부터 타로를 좋아한 사람이 있었다?");
         req.setAttribute("ogDescription", "타로타로 타로 밀크티!!!");
-        req.setAttribute("ogImageUrl", "%s/assets/%s".formatted(req.getContextPath(), "taro.jpeg"));
+        req.setAttribute("ogImageUrl", "%s/assets/%s".formatted(req.getContextPath(), "ai-taro.jpg"));
         req.setAttribute("ogPageUrl", req.getContextPath());
         view(req, resp, "form");
     }
@@ -50,6 +54,11 @@ public class TaroFormController extends Controller {
         // 겹칠 확률이 극도로 낮은 임의의 문자열 값
         String uuid = UUID.randomUUID().toString();
         // 새로운 호출로 처리하겠네...
+        try {
+            supabaseService.save(uuid, response.data(), "");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         resp.sendRedirect(req.getContextPath() + "/result/%s".formatted(uuid));
     }
